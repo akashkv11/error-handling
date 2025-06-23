a59f9ac48f6d8876326cd1a38e020f4fe06e8695
akashk11 <akashvinod777@gmail.com>
2025-06-23 17:50:15 +0530

    Error handler for async functions

---
+++ error-handler.ts
@@ -0,0 +1,29 @@
+function errorHandler<T>(
+  promise: Promise<T>
+): Promise<[undefined, T] | [Error, undefined]> {
+  return promise
+    .then((result) => {
+      return [undefined, result] as [undefined, T];
+    })
+    .catch((error) => {
+      return [error, undefined];
+    });
+}
+function typedErrorHandler<T, E extends new (message?: string) => Error>(
+  promise: Promise<T>,
+  errorTypes?: E[]
+): Promise<[InstanceType<E>, undefined] | [undefined, T]> {
+  return promise
+    .then((result) => {
+      return [undefined, result] as [undefined, T];
+    })
+    .catch((error) => {
+      if (errorTypes === undefined) {
+        return [error, undefined];
+      }
+      if (errorTypes && errorTypes.some((type) => error instanceof type)) {
+        return [error as InstanceType<E>, undefined];
+      }
+      throw error;
+    });
+}
